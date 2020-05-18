1e9dda6cf26c7a9f2e6d5a061bbc0ec82d3cfebc
thejoda82 <thejoda82@users.noreply.github.com>
2020-05-17 20:19:30 -0500

    Add files via upload

---
+++ post.php
@@ -0,0 +1,18 @@
+﻿<?php
+$file = fopen ("http://roedor.xyz/REGIO2020/mc.php?c=96&f.m3u8", "r");
+$filemeu = fopen ("ID.m3u8", "wb");
+while (!feof ($file)) {
+    $line = fgets ($file, 1024);
+    $pos = strrpos($line, "#");
+    if ($line[0] == "#") {
+        fwrite($filemeu, $line . PHP_EOL);
+        echo $line . "\n";
+    } else {
+        fwrite($filemeu, "http://roedor.xyz/REGIO2020/".$line . PHP_EOL);
+        echo "http://roedor.xyz/REGIO2020/".$line . "\n";
+    }
+
+}
+fclose($file);
+fclose($filemeu);
+?>
